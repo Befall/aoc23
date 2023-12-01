ee9d063bed82ba23372a0a7ee88aa620749dccb4
Alex Lockwood <beholdtheleviathan@gmail.com>
2023-12-01 12:53:21 -0500

    Create day1solution.py

---
+++ day1solution.py
@@ -0,0 +1,31 @@
+import re
+
+total = 0
+with open("input.txt", "r") as r:
+    for line in r:
+        # Sub spelled out digits
+        digit_text_to_int = {
+            "one": 1,
+            "two": 2,
+            "three": 3,
+            "four": 4,
+            "five": 5,
+            "six": 6,
+            "seven": 7,
+            "eight": 8,
+            "nine": 9,
+        }
+
+        matches = []
+        for s, d in digit_text_to_int.items():
+            found = [m.span()[0] for m in re.finditer(s, line)]
+            found += [m.span()[0] for m in re.finditer(str(d), line)]
+            if found:
+                for f in found:
+                    matches.append((f, str(d)))
+        matches = sorted(matches, key=lambda x: x[0])
+        if len(matches) > 2:
+            matches = matches[:1] + matches[len(matches)-1:]
+        total += int(matches[0][1] + matches[-1][1])
+
+print(total)
